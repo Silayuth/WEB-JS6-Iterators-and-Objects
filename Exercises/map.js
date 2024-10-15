6750a826655457085449bb388a1cdf7811f56711
Khem <tempkhem@gmail.com>
2024-10-15 10:02:54 +0700

    feat(exercise): map,js

---
+++ Exercises/map.js
@@ -0,0 +1,6 @@
+
+const genH = [ 'Ben', 'Ice', 'Nut', 'Chai', 'Tum' ];
+
+// TASK1: print all characters of genH like 'BenH IceH NutH ....';
+// TASK2: assign new variable name genHnewArray using MAP [ 'Ben Inw', 'Ice Inw', 'Nut Inw', 'Chai Inw', 'Tum Inw' ]
+// TASK3: assign new variable name genHElemets using MAP ['<h1>Ben</h1>', '<h1>Ice</h1>', ...];
